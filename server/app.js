9090798ce0bdc4ba5abeec367d4c7558d44fe7d5
didagu <didagou@gmail.com>
2019-05-15 12:30:19 +0200

    Install and setup graphql and express-graphql

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const graphqlHTTP =require('express-graphql');
 
 const app = express();
+
+//setting middleware
+app.use('/graphql',graphqlHTTP({
+
+}))
 
 app.listen(3000,()=> {
     console.log(`Now listening for request on port 3000`)
 })
+    
