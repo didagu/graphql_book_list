985272a58fc20c9260e15ef2d228eea8d5df6608
didagu <didagou@gmail.com>
2019-05-16 20:21:53 +0200

    Write RootQuery code and Export schema

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const graphqlHTTP =require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 //setting middleware
 app.use('/graphql',graphqlHTTP({
-
+    schema
 }))
 
 app.listen(3000,()=> {
     console.log(`Now listening for request on port 3000`)
 })
-    
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,35 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString , GraphQLSchema } = graphql;
 
+//Define schema types
 const BookType = new GraphQLObjectType({
     name: 'Book',
-    fields: () => ({
+    fields:() => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
 });
+
+//Define root queries
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book : {
+            type: BookType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                //write to retrieve data from db or other sources
+
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
+
