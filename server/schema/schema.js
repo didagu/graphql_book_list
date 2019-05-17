e4142f812a920af67a7414bc0f3dc57e6dfed15d
didagu <didagou@gmail.com>
2019-05-17 14:52:42 +0200

    Define author schema type and Define relations between author and book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,55 @@
 const graphql = require('graphql');
+const _= require('lodash');
 
-const { GraphQLObjectType, GraphQLString , GraphQLSchema } = graphql;
+const { 
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
+
+//dummy data
+let books = [
+    {name:"skdjfsbdfk", genre: "ssdfsfd", id: "1",authorId:"1"},
+    {name:"skdjfsbdfl", genre: "ssdfsfe", id: "2",authorId:"2"},
+    {name:"skdjfsbdfm", genre: "ssdfsff", id: "3",authorId:"3"}
+]
+
+let authors = [
+    {name:"guy q", age: 12, id: "1"},
+    {name:"guy 2", age: 34, id: "2"},
+    {name:"guy 6", age: 43, id: "3"}
+]
 
 //Define schema types
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields:() => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                return _.find(authors, {id: parent.authorId})
+            }
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields:() => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: BookType,
+            resolve(parent, args) {
+                return _.find(books, {authorId: parent.id})
+            }
+        }
     })
 });
 
@@ -19,11 +60,18 @@ const RootQuery = new GraphQLObjectType({
         book : {
             type: BookType,
             args: {
-                id: { type: GraphQLString }
+                id: { type: GraphQLID }
             },
             resolve(parent, args) {
-                //write to retrieve data from db or other sources
-
+                //write code to retrieve data from db or other sources
+                return _.find(books, {id: args.id});
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return _.find(authors, {id:args.id});
             }
         }
     }
